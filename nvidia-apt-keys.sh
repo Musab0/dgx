dbe8cb73a4635fc39618eac4e776e386bc64503f
Musab <musab.obaid@gmail.com>
2023-10-31 09:21:59 +0400

    Update nvidia-apt-keys.sh

--- nvidia-apt-keys.sh
+++ nvidia-apt-keys.sh
@@ -1,3 +1,5 @@
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-wget -qO - https://repo.download.nvidia.com/baseos/ubuntu/focal/X86_64/7fa2af80.pub | sudo apt-key add -
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo apt-key add 7fa2af80.pub
+wget https://repo.download.nvidia.com/baseos/ubuntu/focal/X86_64/7fa2af80.pub
+sudo apt-key add 7fa2af80.pub
 sudo apt-get update
